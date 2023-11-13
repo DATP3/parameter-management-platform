7f03de05bb3501a486c1c769e37fb6efc484a5fb
emil091001 <emil091001@gmail.com>
2023-11-13 14:07:10 +0100

    code formating, to make it more readable

--- pmp-client/src/features/parameters/Input.tsx
+++ pmp-client/src/features/parameters/Input.tsx
@@ -15,50 +15,57 @@ const Input = (props: InputProps) => {
 	const { type, isValid, disabled, value, onParamChange } = props;
 
 	if (InputTextFieldTypes.includes(type))
-		return (<TextField
-			invalid={!isValid}
-			disabled={disabled}
-			type={TextTypes.includes(type) ? "text" : "number"}
-			outlined
-			className={"parameterInput" + (isValid ? " " : " invalid ")}
-			value={value as string}
-			onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-				onParamChange(e.target.value)
-			}}
-		/>);
+		return (
+			<TextField
+				invalid={!isValid}
+				disabled={disabled}
+				type={TextTypes.includes(type) ? "text" : "number"}
+				outlined
+				className={"parameterInput" + (isValid ? " " : " invalid ")}
+				value={value as string}
+				onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+					onParamChange(e.target.value)
+				}}
+			/>);
+
 
 	if (type === ParameterType.LOCALDATETIME)
-		return (<TextField
-			outlined
-			invalid={!isValid}
-			className="parameterInput"
-			type="datetime-local"
-			value={value as string}
-			onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-				onParamChange(e.target.value);
-			}} />);
+		return (
+			<TextField
+				outlined
+				invalid={!isValid}
+				className="parameterInput"
+				type="datetime-local"
+				value={value as string}
+				onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+					onParamChange(e.target.value);
+				}}
+			/>);
 
 	if (type === ParameterType.LOCALDATE)
-		return (<TextField
-			outlined
-			invalid={!isValid}
-			className="parameterInput"
-			type="date"
-			value={value as string}
-			onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-				console.log(e.target.value);
-				onParamChange(e.target.value);
-			}} />
-		);
+		return (
+			<TextField
+				outlined
+				invalid={!isValid}
+				className="parameterInput"
+				type="date"
+				value={value as string}
+				onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+					console.log(e.target.value);
+					onParamChange(e.target.value);
+				}}
+			/>);
 
 	if (type === ParameterType.BOOLEAN) {
 		const boolValue = value === "true";
-		return <Switch
-			label={value as string}
-			checked={boolValue}
-			onChange={(e: any) => {
-				onParamChange(e.target.checked ? "true" : "false");
-			}} />
+		return (
+			<Switch
+				label={value as string}
+				checked={boolValue}
+				onChange={(e: any) => {
+					onParamChange(e.target.checked ? "true" : "false");
+				}}
+			/>);
 	}
 
 
