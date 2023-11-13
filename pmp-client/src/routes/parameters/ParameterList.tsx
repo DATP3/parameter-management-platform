0b4119cbbaf12db4595ef9817efafbb5d372ff47
emil091001 <emil091001@gmail.com>
2023-11-13 10:37:37 +0100

    fixed ParameterList

--- pmp-client/src/routes/parameters/ParameterList.tsx
+++ pmp-client/src/routes/parameters/ParameterList.tsx
@@ -14,6 +14,59 @@ const ParameterList = (props: ParameterListProps) => {
     const [sortingCatagory, setSortingCatagory] = useState<SortingCatagory>(SortingCatagory.NAME);
     const [sortingDirection, setSortingDirection] = useState(1);
 
+	const sort = (parameters: Parameter[], catagory: string) => {
+		if (catagory === SortingCatagory.Name)
+			return parameters.sort((a, b) => a.name.localeCompare(b.name) * sortingDirection);
+
+		if (catagory === SortingCatagory.Type)
+			return parameters.sort((a, b) => a.type.localeCompare(b.type) * sortingDirection);
+
+		if (catagory === SortingCatagory.Value)
+			return parameters.sort((a, b) => (a.value as string).localeCompare(b.value as string) * sortingDirection);
+
+		return parameters;
+	}
+
+	const sortedParameters = sort(parameters, sortingCatagory);
+
+	return (
+		<DataTable className="parameterTable">
+			<DataTableContent className="tableHead">
+				<DataTableHead >
+					<DataTableRow>
+						<DataTableHeadCell
+							className="headCell"
+							sort={sortingCatagory === SortingCatagory.Name ? sortingDirection : 0}
+							onSortChange={() => setSortingDirection(sortingDirection * -1)}
+							onClick={() => setSortingCatagory(SortingCatagory.Name)}
+						>
+							Name
+						</DataTableHeadCell>
+						<DataTableHeadCell
+							className="headCell"
+							sort={sortingCatagory === SortingCatagory.Type ? sortingDirection : 0}
+							onSortChange={() => setSortingDirection(sortingDirection * -1)}
+							onClick={() => setSortingCatagory(SortingCatagory.Type)}
+						>
+							Type
+						</DataTableHeadCell>
+						<DataTableHeadCell
+							className="headCell"
+							sort={sortingCatagory === SortingCatagory.Value ? sortingDirection : 0}
+							onSortChange={() => setSortingDirection(sortingDirection * -1)}
+							onClick={() => setSortingCatagory(SortingCatagory.Value)}
+						>
+							Value
+						</DataTableHeadCell>
+					</DataTableRow>
+				</DataTableHead>
+				<DataTableBody>
+					{sortedParameters.map((parameter) => (
+						<ParameterListRow service={service} parameter={parameter} />
+					))}
+				</DataTableBody>
+			</DataTableContent>
+		</DataTable>
     const sort = (parameters: Parameter[], catagory: SortingCatagory, sortingDirection: number) => {
         if (catagory === SortingCatagory.NAME)
             return parameters.sort((a, b) => a.name.localeCompare(b.name) * sortingDirection);
