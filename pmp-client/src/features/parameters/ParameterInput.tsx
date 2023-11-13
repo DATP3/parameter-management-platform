b44c32f64a99bf1112fb92dd83f6780db250faaa
emil091001 <emil091001@gmail.com>
2023-11-13 10:49:42 +0100

    added general parameter input

---
+++ pmp-client/src/features/parameters/ParameterInput.tsx
@@ -0,0 +1,32 @@
+import { Parameter } from "../../features/parameters/types";
+import useCommitStore from "../../features/changes/useCommitStore";
+import { Service } from "../../features/services/types";
+import { ParameterValue } from "../../features/changes/types";
+import validateParamChange from "../../features/changes/validateParamChange";
+import Input from "./Input";
+import "./style.css"
+
+interface ParameterInputProps {
+	parameter: Parameter;
+	service: Service;
+}
+
+const ParameterInput = (props: ParameterInputProps) => {
+	const { parameter, service } = props;
+	const addParameterChange = useCommitStore((s) => s.addParameterChange);
+
+	const parameterChange = useCommitStore((s) => s.findParameterChange(service, parameter));
+	const hasChange = parameterChange !== undefined;
+	const value = hasChange ? parameterChange.newValue : parameter.value;
+	const isValid = validateParamChange({ parameter, newValue: value });
+
+	const handleParameterChange = (newValue: ParameterValue) => {
+		addParameterChange(service, { parameter, newValue });
+	};
+
+	return (
+		<Input isValid={isValid} value={value} type={parameter.type} onParamChange={handleParameterChange} />
+	);
+}
+
+export default ParameterInput;
