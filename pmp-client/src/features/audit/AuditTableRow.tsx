da4d07b0b7554206c20d9c8ffe8b1ef70db3166a
Bliztle <asbjoern.r.e@gmail.com>
2023-11-20 12:07:29 +0100

    simplifyed commit store. Now follows model using predicates

--- pmp-client/src/features/audit/AuditTableRow.tsx
+++ pmp-client/src/features/audit/AuditTableRow.tsx
@@ -4,6 +4,26 @@ import AuditDetailsDialog from './AuditDetailsDialog';
 import { AuditLogEntry } from './useAuditLogEntries';
 import { useState } from 'react';
 
+// interface AuditTableRowRevertProps {
+//     entry: AuditLogEntry;
+// }
+
+// const AuditTableRowRevert = ({ entry }: AuditTableRowRevertProps) => {
+//     // const addRevert = useCommitStore(s => s.addRevert);
+
+//     const handleClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+//         e.stopPropagation();
+//     };
+
+//     return (
+//         <>
+//             <Tooltip content='Revert commit'>
+//                 <IconButton icon={{ icon: 'undo' }} onClick={handleClick} />
+//             </Tooltip>
+//         </>
+//     );
+// };
+
 interface AuditTableRowProps {
     entry: AuditLogEntry;
 }
@@ -18,6 +38,9 @@ const AuditTableRow = ({ entry }: AuditTableRowProps) => {
                 <DataTableCell>{entry.email}</DataTableCell>
                 <DataTableCell>{entry.hash}</DataTableCell>
                 <DataTableCell>{entry.message}</DataTableCell>
+                {/* <DataTableCell>
+                    <AuditTableRowRevert entry={entry} />
+                </DataTableCell> */}
             </DataTableRow>
         </>
     );
