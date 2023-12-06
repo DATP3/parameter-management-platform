cdf7679faf4c4e5c4bbadace4bb4bbc5b3da7925
emil091001 <emil091001@gmail.com>
2023-12-06 13:17:38 +0100

    Made test for revert deserialiser (NOT WORKING)

--- pmp-core/src/main/java/dk/nykredit/pmp/core/remote/json/RevertAdapter.java
+++ pmp-core/src/main/java/dk/nykredit/pmp/core/remote/json/RevertAdapter.java
@@ -2,6 +2,10 @@ package dk.nykredit.pmp.core.remote.json;
 
 import java.io.IOException;
 
+import javax.inject.Inject;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import dk.nykredit.pmp.core.audit_log.AuditLog;
 import dk.nykredit.pmp.core.audit_log.ChangeType;
 import dk.nykredit.pmp.core.commit.Change;
@@ -10,15 +14,21 @@ import dk.nykredit.pmp.core.commit.CommitRevert;
 import dk.nykredit.pmp.core.commit.ParameterChange;
 import dk.nykredit.pmp.core.commit.RevertFactory;
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 public class RevertAdapter {
     private String parameterName;
     private String revertType;
     private String commitReference;
 
-    public Change toRevert(AuditLog auditLog) throws IOException {
-        long commitHash = Long.decode("0x" + commitReference);
+    @JsonIgnore
+    @Inject
+    AuditLog auditLog;
+
+    public Change toRevert() throws IOException {
+        long commitHash = Long.parseUnsignedLong(commitReference, 16);
 
         if (revertType.equals("parameter")) {
             Commit commit = auditLog.getCommit(commitHash);
