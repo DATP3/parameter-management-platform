3534bd32064151d024bdbf4b565ee85a58436208
Markus Dam <markus.dam123@gmail.com>
2023-12-19 10:09:13 +0100

    comments

--- pmp-core/src/main/java/dk/nykredit/pmp/core/remote/PMPHandlerFactory.java
+++ pmp-core/src/main/java/dk/nykredit/pmp/core/remote/PMPHandlerFactory.java
@@ -3,5 +3,9 @@ package dk.nykredit.pmp.core.remote;
 import org.eclipse.jetty.server.Handler;
 
 public interface PMPHandlerFactory {
+    /**
+     * Get the handler for a Jetty server to host PMP
+     * @return The handler that exposes the PMP API
+     */
     Handler getHandler();
 }
